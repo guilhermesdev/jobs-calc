92c7bf23f5d62606097a41900db452b03501e131
Guilherme Souza <dev.guilhermesouza@gmail.com>
2021-11-15 11:06:08 -0300

    refact: add prepared statements to SignUp model

--- src/model/SignUp.js
+++ src/model/SignUp.js
@@ -1,11 +1,19 @@
 const Database = require('../db/config');
 
+function randomAvatarImage() {
+	const number = String(Math.random()).replace('.', '');
+
+	return `https://avatars.dicebear.com/api/avataaars/${number}.svg?background=%2371718c`;
+}
+
 module.exports = {
 	async verifyIfEmailExists(email){
 		const db = await Database();
 
-		const response = 
-			await db.get(`SELECT name FROM users WHERE email = "${email}";`);
+		const response = await db.get(
+				`SELECT name FROM users WHERE email = $email;`,
+				{ $email: email }
+			);
 
 		await db.close();
 		return !!response;
@@ -14,27 +22,35 @@ module.exports = {
 		const db = await Database();
 
 		try{
-			await db.run(`INSERT INTO users(
-				name,
-				email,
-				password,
-				avatar,
-				monthly_budget,
-				days_per_week,
-				hours_per_day,
-				vacation_per_year,
-				hour_value
-			) VALUES (
-				"${name}",
-				"${email}",
-				"${passwordHash}",
-				"https://avatars.dicebear.com/api/avataaars/${String(Math.random()).replace('.','')}.svg?background=%2371718c",
-				3000,
-				5,
-				5,
-				4,
-				30
-			);`);
+			await db.run(
+				`INSERT INTO users(
+					name,
+					email,
+					password,
+					avatar,
+					monthly_budget,
+					days_per_week,
+					hours_per_day,
+					vacation_per_year,
+					hour_value
+				) VALUES (
+					$name,
+					$email,
+					$passwordHash,
+					$avatarImage,
+					3000,
+					5,
+					5,
+					4,
+					30
+				);`,
+				{
+					$name: name,
+					$email: email,
+					$passwordHash: passwordHash,
+					$avatarImage: randomAvatarImage()
+				}
+			);
 		} catch {
 			throw new Error('Não foi possível criar o usuário');
 		} finally {
