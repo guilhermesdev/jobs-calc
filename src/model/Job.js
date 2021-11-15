293e7227b7ce9c12b4f2fa02264b710d827ff562
Guilherme Souza <dev.guilhermesouza@gmail.com>
2021-11-15 10:44:56 -0300

    refact: add prepared statements to Job model

--- src/model/Job.js
+++ src/model/Job.js
@@ -7,9 +7,18 @@ module.exports = {
 		let data;
 
 		if(!jobId){
-			data = await db.all(`SELECT * FROM jobs WHERE user_id = ${userId}`);
+			data = await db.all(
+				`SELECT * FROM jobs WHERE user_id = $userId`,
+				{ $userId: userId }
+			);
 		} else {
-			data = await db.get(`SELECT * FROM jobs WHERE user_id = ${userId} AND job_id = ${jobId}`)
+			data = await db.get(
+				`SELECT * FROM jobs WHERE user_id = $userId AND job_id = $jobId`,
+				{
+					$userId: userId,
+					$jobId: jobId
+				}
+			);
 		}
 
 		await db.close();
@@ -19,38 +28,60 @@ module.exports = {
 	async update(job, jobId){
 		const db = await Database();
 
-		await db.run(`UPDATE jobs SET
-			name = "${job.name}",
-			daily_hours = ${job.daily_hours},
-			total_hours = ${job.total_hours}
-			WHERE job_id = ${jobId};
-		`);
+		await db.run(
+			`UPDATE jobs SET
+			name = $name,
+			daily_hours = $dailyHours,
+			total_hours = $totalHours
+			WHERE job_id = $jobId;`,
+			{
+				$name: job.name,
+				$dailyHours: job.daily_hours,
+				$totalHours: job.total_hours,
+				$jobId: jobId
+			}
+		);
 
 		await db.close();
 	},
 	async delete(id, userId){
 		const db = await Database();
 
-		await db.run(`DELETE FROM jobs WHERE job_id = ${id} AND user_id = ${userId}`);
+		await db.run(
+			`DELETE FROM jobs WHERE job_id = $id AND user_id = $userId`,
+			{
+				$id: id,
+				$userId: userId
+			}
+		);
 
 		await db.close();
 	},
 	async create(newJob){
 		const db = await Database();
 
-		await db.run(`INSERT INTO jobs(
-			name,
-			user_id,
-			daily_hours,
-			total_hours,
-			created_at
-		) VALUES (
-			"${newJob.name}",
-			${newJob.user_id},
-			${newJob.daily_hours},
-			${newJob.total_hours},
-			${newJob.created_at}
-		);`)
+		await db.run(
+			`INSERT INTO jobs(
+				name,
+				user_id,
+				daily_hours,
+				total_hours,
+				created_at
+			) VALUES (
+				$name,
+				$userId,
+				$dailyHours,
+				$totalHours,
+				$createdAt
+			);`,
+			{
+				$name: newJob.name,
+				$userId: newJob.user_id,
+				$dailyHours: newJob.daily_hours,
+				$totalHours: newJob.total_hours,
+				$createdAt: newJob.created_at
+			}
+		);
 
 		await db.close();
 	}
